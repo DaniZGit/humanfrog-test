e93d395f9117e6c75c1b66f2774908cf4223dea8
Daniel <danzaric10@gmail.com>
2025-03-16 15:59:55 +0100

    Displaying dashed box on large screens near the header area

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="max-w-screen-lg mx-auto overflow-hidden relative font-verb">
-    <Header :title="headerTitle"></Header>
-    <div class="md:grid grid-cols-12">
+  <div class="overflow-hidden relative font-verb">
+    <Header :title="headerTitle" class="max-w-screen-lg mx-auto"></Header>
+    <div class="max-w-screen-lg mx-auto md:grid grid-cols-12 relative">
+      <!-- Dashed background -->
+      <div
+        class="hidden lg:inline absolute -left-[20px] -top-[210px] h-[200px] w-auto aspect-square bg-accent-hover z-50"
+        style="
+          background: repeating-linear-gradient(
+            -45deg,
+            #318565 0px,
+            #318565 2px,
+            transparent 1px,
+            transparent 6px
+          );
+        "
+      ></div>
+
       <div
         class="hidden md:flex flex-col md:col-span-4 lg:col-span-3 px-8 ml-14 relative"
       >
@@ -18,7 +32,7 @@
         </div>
       </div>
     </div>
-    <Footer class="mt-16"></Footer>
+    <Footer class="max-w-screen-lg mx-auto mt-16"></Footer>
   </div>
 </template>
 
